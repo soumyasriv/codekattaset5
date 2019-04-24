234ec24963741e27b4da887ebdc27640a6bf256f
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-04-24 10:06:52 -0700

    Create inclusive_no.java

---
+++ inclusive_no.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if (n>=1 && n<=10)
+	    {
+	        System.out.println("yes");
+	    }
+	    else
+	    System.out.println("no");
+	}
+}
+	    
