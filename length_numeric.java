a29b8990ed018a0c536a96234d7ca83f57b5c56f
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-04-24 10:32:54 -0700

    Create length_numeric.java

---
+++ length_numeric.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    int len=s.length();
+	    System.out.print(len);
+	   
+	}
+}
+	    
+	    
